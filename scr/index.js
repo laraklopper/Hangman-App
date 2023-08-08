23feb0806a285e0c0336053cf1627561749d3c97
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-08-08 13:31:42 +0200

    Update index.js

--- scr/index.js
+++ scr/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';// Import the React module to use react functionalities
+import ReactDOM from 'react-dom/client';// Import the ReactDOM library, to provide a method rendering React components into the DOM
+import './index.css';//Import the CSS file
+import App from './App';//Import the App component 
+import 'bootstrap/dist/css/bootstrap.min.css';//Import React Bootstrap Library
+import reportWebVitals from './reportWebVitals';//Import reportWebVitals function from './reportWebVitals'
 
+// Create a root for rendering the React app using the element id
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Render the App component inside the root using ReactDOM
 root.render(
   <React.StrictMode>
     <App />
